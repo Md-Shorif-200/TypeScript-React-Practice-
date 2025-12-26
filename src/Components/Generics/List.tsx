85075030b9a038fc9bdd9957407532e8f7f910c9
Md Shorif Uddin <mdshorifuddin463@gmail.com>
2025-12-26 13:10:52 -0800

    -

--- src/Components/Generics/List.tsx
+++ src/Components/Generics/List.tsx
@@ -1,12 +1,12 @@
 
- type ListPropsType = {
-      items : string[];
-      onClick : (value:string) => void
+ type ListPropsType<T> = {
+      items : T[],
+      onClick : (value :T) => void
  }
 
 
 
-export default function List({items,onClick}:ListPropsType) {
+export default function List<T extends {id : number,name : string,}>({items,onClick}:ListPropsType<T>) {
   return (
     <div>
          <h1> List of Item </h1>
@@ -14,7 +14,7 @@ export default function List({items,onClick}:ListPropsType) {
              {
                  items.map((item,index) => {
                      return (
-                          <h2 key={index} onClick={() => onClick(item)} > {item} </h2>
+                          <h2 key={index} onClick={() => onClick(item)} > {item.id} {item.name}</h2>
                      )
                  })
              }
